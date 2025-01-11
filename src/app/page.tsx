7356f810efa51f3e05c16fdb1022b0bcb0a977eb
QuangAnhTo <quangato072@gmail.com>
2025-01-11 10:31:01 +0700

    feat: enhance Home component layout with improved styling for charts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-'use client';
-import ProjectStat from '@/components/ProjectsPage/ProjectStat';
-import React from 'react';
-import ReactApexChart from 'react-apexcharts'; // Import ReactApexChart
+"use client";
+import ProjectStat from "@/components/ProjectsPage/ProjectStat";
+import React from "react";
+import ReactApexChart from "react-apexcharts"; // Import ReactApexChart
 
 interface ChartProps {
   series: {
@@ -15,28 +15,42 @@ interface ChartProps {
 
 const Home: React.FC<ChartProps> = () => {
   const options = {
-    series: [{
-      name: 'sales',
-      data: [30, 40, 35, 50, 49, 60, 70, 91, 125]
-    }],
+    series: [
+      {
+        name: "sales",
+        data: [30, 40, 35, 50, 49, 60, 70, 91, 125],
+      },
+    ],
     xaxis: {
-      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999]
-    }
+      categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999],
+    },
   };
 
   const pieOptions = {
     series: [44, 55, 13, 43, 22],
     chartOptions: {
-      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"]
-    }
+      labels: ["Team A", "Team B", "Team C", "Team D", "Team E"],
+    },
   };
 
   return (
     <div className="h-full">
-      <h1 className="text-2xl font-semibold text-gray-800">Sales Chart</h1>
-      <div className="h-1/4">
-        <ReactApexChart options={options} series={options.series} type="line" height="350" />
-        <ProjectStat series={pieOptions.series} chartOptions={pieOptions.chartOptions}/>
+      <div className="flex flex-row gap-8  h-1/4 items-start justify-start p-10">
+        <div className="bg-white rounded-md shadow-md p-4">
+          <h1 className="text-2xl font-semibold text-gray-800">Sales Chart</h1>
+          <ReactApexChart
+            options={options}
+            series={options.series}
+            type="line"
+            width="400"
+          />
+        </div>
+        <div className="bg-white rounded-md shadow-md p-4">
+          <ProjectStat
+            series={pieOptions.series}
+            chartOptions={pieOptions.chartOptions}
+          />
+        </div>
       </div>
     </div>
   );
