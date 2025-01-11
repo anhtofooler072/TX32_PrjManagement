530a793d6a3903cb3f159a2c463c5047594ca1a9
QuangAnhTo <quangato072@gmail.com>
2025-01-11 10:31:15 +0700

    feat: update ProjectStat component with improved layout and chart width

--- src/components/ProjectsPage/ProjectStat.tsx
+++ src/components/ProjectsPage/ProjectStat.tsx
@@ -1,6 +1,6 @@
-'use client';
-import React from 'react';
-import ReactApexChart from 'react-apexcharts';
+"use client";
+import React from "react";
+import ReactApexChart from "react-apexcharts";
 
 interface ChartProps {
   series: number[];
@@ -11,10 +11,15 @@ interface ChartProps {
 
 const ProjectStat: React.FC<ChartProps> = ({ series, chartOptions }) => {
   return (
-    <div className="h-full">
-      <h1 className="text-2xl font-semibold text-gray-800">Sales Chart</h1>
-      <div className="h-1/4">
-        <ReactApexChart options={chartOptions} series={series} type="donut" height="350" />
+    <div>
+      <h1 className="text-2xl font-semibold text-gray-800">Performance</h1>
+      <div>
+        <ReactApexChart
+          options={chartOptions}
+          series={series}
+          type="donut"
+          width="400"
+        />
       </div>
     </div>
   );
